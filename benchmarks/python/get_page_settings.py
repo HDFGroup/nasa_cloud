44605f0c4e753d092fb45c6d54d26cde179ac8c2
jreadey <jreadey@hdfgroup.org>
2023-05-17 13:51:58 +0000

    add get_page_settings.py

---
+++ benchmarks/python/get_page_settings.py
@@ -0,0 +1,42 @@
+import sys
+import os
+import h5py
+import s3fs
+
+
+if len(sys.argv) < 2 or sys.argv[1] in ("-h", "--help"):
+    sys.exit(f"usage: python {sys.argv[0]} <filepath or s3uri>")
+
+print(f"h5py version: {h5py.version.version}")
+print(f"hdf5 version: {h5py.version.hdf5_version}")
+
+filepath = sys.argv[1]
+
+kwargs = {}
+
+ros3_params = {"aws_region": "AWS_REGION",
+               "secret_id": "AWS_ACCESS_KEY_ID",
+               "secret_key": "AWS_SECRET_ACCESS_KEY"}
+
+if filepath.startswith("s3://"):
+    s3 = s3fs.S3FileSystem()
+    f = h5py.File(s3.open(filepath, 'rb'), **kwargs)
+elif filepath.startswith("http"):
+    kwargs["driver"] = "ros3"
+    for ros3_param in ros3_params:
+        env_name = ros3_params[ros3_param]
+        if env_name not in os.environ:
+            sys.exit(f"env variable: {env_name} must be set for ros3")
+        env_value = os.environ[env_name]
+        kwargs[ros3_param] = env_value.encode("utf-8")
+    print("kwargs:", kwargs)
+    f = h5py.File(filepath, **kwargs)
+else:
+    f = h5py.File(filepath, **kwargs)
+    
+print(f"got file: {f}")
+fapl = f.id.get_access_plist()
+page_buffer_size = fapl.get_page_buffer_size()
+print(f"page buffer size: {page_buffer_size}")
+f.close()
+
